cd6dee7991284b4948fe8e50588858ee253c2328
RockosBos <32206608+RockosBos@users.noreply.github.com>
2022-08-08 20:45:01 -0400

    Version Before Node

--- Project-1-Survey-Form/index.js
+++ Project-1-Survey-Form/index.js
@@ -1,4 +1,19 @@
-const fileSystem = require("browserify-fs")
+var barColors = [
+	"rgba(0, 0, 255, 1.0)",
+	"rgba(0, 255, 0, 1.0)",
+	"rgba(255, 0, 0, 1.0)",
+
+];
+var q1xValues = [];
+var q1yValues = [];
+
+
+
+var obj = {
+	table: []
+};
+
+const fs = require("fs")
 var currentQuestion;
 
 function onLoad(e){
@@ -31,13 +46,109 @@ function handleSubmit(event){
 	$("#questionBar").hide();
 	$("#questions").hide();
 	$("#NavBar").hide();
+
+	writeToJSON(values);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
 	const form = document.querySelector("form");
 	form.addEventListener('submit', handleSubmit);
+	var charts = [
+		new Chart("q1Chart", {
+			type: "pie",
+			data: {
+				labels: q1xValues,
+				datasets: [{
+					backgroundColor: barColors,
+					data: q1yValues
+				}]
+			},
+			options: {
+				title:{
+					display: true,
+					text: "Question 1: What is your favorite color?"
+				}
+			}
+		}),
+	
+		new Chart("q2Chart", {
+			type: "pie",
+			data: {
+				labels: q2xValues,
+				datasets: [{
+					backgroundColor: barColors,
+					data: q2yValues
+				}]
+			},
+			options: {
+				title:{
+					display: true,
+					text: "Question 2: "
+				}
+			}
+		}),
+	
+		new Chart("q1Chart", {
+			type: "pie",
+			data: {
+				labels: q3xValues,
+				datasets: [{
+					backgroundColor: barColors,
+					data: q3yValues
+				}]
+			},
+			options: {
+				title:{
+					display: true,
+					text: "Question 3: "
+				}
+			}
+		}),
+	
+		new Chart("q1Chart", {
+			type: "pie",
+			data: {
+				labels: q4xValues,
+				datasets: [{
+					backgroundColor: barColors,
+					data: q4yValues
+				}]
+			},
+			options: {
+				title:{
+					display: true,
+					text: "Question 4: "
+				}
+			}
+		}),
+	
+		new Chart("q1Chart", {
+			type: "pie",
+			data: {
+				labels: q5xValues,
+				datasets: [{
+					backgroundColor: barColors,
+					data: q5yValues
+				}]
+			},
+			options: {
+				title:{
+					display: true,
+					text: "Question 5: "
+				}
+			}
+		})
+	];
 });
 
 function writeToJSON(values){
-	const fileSystem = require("browserify-fs")
+	
+	values.forEach(obj.table.push(values[i]));
+	var json = JSON.stringify(obj);
+	fs.writeFile('results.json', json, 'utf8', callback);
+
+}
+
+function fillResults(){
+	
 }
